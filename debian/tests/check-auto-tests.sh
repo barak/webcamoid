e9e380f19dd5c1d0a7a4d990a8b175c7005d0765
Herbert Parentes Fortes Neto <hpfn@debian.org>
2018-11-28 09:03:14 -0200

    CI tests - use shunit2

--- debian/tests/check-auto-tests.sh
+++ debian/tests/check-auto-tests.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
+exec 2>&1
+
 set -e
 
 debian/rules override_dh_auto_configure
 dh_auto_build -a -O--buildsystem=qmake
 
-if [ ! -f 'libAvKys/config.log' ]
-then
-	exit 1
-fi
+test_exists() {
+    #assertTrue "[ -f 'libAvKys/config.log' ]"
+    log_file_exists="$(test -f libAvKys/config.log && echo 'file exists')"
+    assertEquals 'file exists' "$log_file_exists"
+}
 
-quant_test_auto="$(find ./ -name test_auto | wc -l)"
-quant_succeeded="$(grep succeeded libAvKys/config.log | wc -l)"
+test_equals() {
+    quant_test_auto="$(find ./ -name test_auto | wc -l)"
+    quant_succeeded="$(grep succeeded libAvKys/config.log | wc -l)"
+    assertEquals "$quant_test_auto" "$quant_succeeded"
+}
 
-if [ "$quant_test_auto" -ne "$quant_succeeded" ]
-then
-    exit 1
-fi
-
-echo "$quant_test_auto == $quant_succeeded"
-
-exit 0
+. shunit2
